e3caf1cfa936eba2c884032f2d30bf3314e10893
guvenyilmaz <guvenyilmaz@gmail.com>
2019-10-26 17:50:53 -0500

    Read me is updated Imports are cleaned

--- src/main/java/com/ecommerce/security/JWTAuthenticationFilter.java
+++ src/main/java/com/ecommerce/security/JWTAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package com.ecommerce.security;
 
 import com.auth0.jwt.JWT;
-import com.ecommerce.controllers.UserController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
